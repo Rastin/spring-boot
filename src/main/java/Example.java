467e603ceeecfec755e058384a34dcec579ca808
Rastin <rastin.ken@gmail.com>
2021-01-05 12:22:53 -0800

    Update Example.java
    DEV to PROD!

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "OpenShift Spring Boot DEV v1.0";
+        return "OpenShift Spring Boot PROD v1.0";
     }
 
     public static void main(String[] args) throws Exception {
